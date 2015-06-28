672f9ce3e8423f8e52835c902fe87cd22c8ca45c
sinkcup <sinkcup@163.com>
2015-06-28 22:32:07 +0800

    use files.md and dirs.md, use size and md5

--- download.sh
+++ download.sh
@@ -2,14 +2,19 @@
 set -e
 top_dir=$(cd `dirname $0`; pwd)
 echo $top_dir
+source $top_dir/dl.conf
+echo $bucket
+echo $domain
 
 function download()
 {
-    files=`find $1 -name 'origin.md'`
+    dl_dir=$1
+    cd $dl_dir
+    files=`find $dl_dir -name 'origin.md'`
     for file in $files; do
         absolute_dir=`dirname $file`
         echo $absolute_dir
-        relative_dir=`echo $file | sed -e "s|$top_dir||" | xargs dirname`
+        relative_dir=`echo $file | sed -e "s|$dl_dir||" | xargs dirname`
         echo $relative_dir
         
         #如果需要检查md5
@@ -20,58 +25,73 @@ function download()
         fi
 
         thead_line_num=`grep -n "\-|\-" $file | awk -F: '{print $1}'`
-        echo 'filename|md5sum' > $absolute_dir/files.md
-        echo '--------|------' >> $absolute_dir/files.md
         offset=$(($thead_line_num+1))
         tail -n +$offset $file | while read line; do
             i=0
             new_line=""
             http_code=200
+            size=""
+            md5=""
             for part in `echo $line | sed 's/|/ /g'`; do
-                echo $i
                 echo $part
                 #第一列必须是下载地址
                 if [ $i -eq 0 ]; then
                     uri=$part
                     origin_filename=`basename $uri`
+                    filename=$origin_filename
+                    uri_nopro=${part#*//}
+                    target_path=${uri_nopro#*/}
                 elif [ $i -eq 1 ]; then
-                    #第2列必须是文件名，如果为空的话，将使用下载地址里相同的文件名
-                    #todo 如果为空的话，for循环结束了，不会进到这里……
-                    if [ $part = '' ]; then
-                        filename=$origin_filename
-                    else
-                        filename=$part
-                    fi
-                    http_code=`curl -sI "http://downloads.openwrt.io$relative_dir/$filename" | head -n 1 | awk '{print $2}'`
-                    # 把文件名都改成链接
-                    new_line=$filename
-                    if [ $http_code -ne 200 ]; then
-                        if [ ! -f $top_dir$relative_dir/$filename ]; then
-                            wget -O $top_dir$relative_dir/$filename $uri
-                        fi
+                    #第2列必须是文件名或路径，如果为空的话，将使用下载地址里相同的文件名
+                    filename=`basename $part`
+                    target_path=.$relative_dir/$part
+                    if [ $relative_dir == '/' ]; then
+                        target_path=$part
                     fi
                 elif [ $i -eq 2 ]; then
-                    #第3列可能是md5sum或者size
-                    if [ $http_code -ne 200 ]; then
-                        if [ $is_check_md5 -eq 1 ]; then
-                            expected_md5=$part
-                            md5=`md5sum $top_dir$relative_dir/$filename | awk '{print $1}'`
-                            if [ $expected_md5 != $md5 ]; then
-                                echo "error: md5 not match"
-                                exit 1
-                            fi
-                        fi
-                    fi
-                    new_line=$new_line'|'$part
-                    echo $new_line >> $top_dir$relative_dir/files.md
+                    #第3列是size
+                    #如果第2列为空，则第3列必须为空，否则第3变成了第2，会错乱
+                    size=$part
+                elif [ $i -eq 3 ]; then
+                    #第4列是md5
+                    expected_md5=$part
                 fi
                 i=$(($i+1))
             done
+            target_dir=""
+            if [ $target_path != $filename ]; then
+                target_dir=${target_path%/*}
+                mkdir -p $target_dir
+            fi
+            echo 'target_path' $target_path
+            echo 'target_dir' $target_dir
+            echo ''
+            if [ ! -f $dl_dir/$target_dir/files.md ]; then
+                echo 'filename|size|md5' > $dl_dir/$target_dir/files.md
+                echo '--------|----|---' >> $dl_dir/$target_dir/files.md
+            fi
+
+            echo "http://$domain/$target_path"
+            http_code=`curl -sI "http://$domain/$target_path" | head -n 1 | awk '{print $2}'`
+            if [ $http_code -ne 200 ]; then
+                if [ ! -f $dl_dir/$target_path ]; then
+                    wget -O $dl_dir/$target_path "$uri"
+                fi
+                size=`ls -lh $dl_dir/$target_path | awk '{print $5}'`
+                md5=`md5sum $dl_dir/$target_path | awk '{print $1}'`
+                if [ $is_check_md5 -eq 1 ] && [ "x"$expected_md5 != "x" ]; then
+                    if [ $expected_md5 != $md5 ]; then
+                        echo "error: md5 not match"
+                        exit 1
+                    fi
+                fi
+            fi
+ 
+            echo ${filename/_/\\_}'|'$size'|'$md5 >> $dl_dir/$target_dir/files.md
         done
     done
     return 0
-    exit
 }
-tmp=$(download $top_dir)
+tmp=$(download $top_dir/dl)
 echo "$tmp"
 exit
--- index.sh
+++ index.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-set -ev
+set -e
+top_dir=$(cd `dirname $0`; pwd)
+echo $top_dir
+dl_dir=$top_dir/dl
+source $top_dir/dl.conf
 
 # 来自环境变量
 u=$qiniu_user
@@ -10,7 +14,7 @@ if [ -z $qiniu_user ]; then
     read -s qiniu_user
     if [ -z $qiniu_user ]; then
         echo '错误：未输入'
-        exit 1
+#        exit 1
     fi
 fi
 
@@ -19,17 +23,14 @@ if [ -z $qiniu_passwd ]; then
     read -s qiniu_passwd
     if [ -z $qiniu_passwd ]; then
         echo '错误：未输入'
-        exit 1
+#        exit 1
     fi
 fi
 
 # 生成index.html
-top_path=$(cd `dirname $0`; pwd)
+#qrsctl login $qiniu_user $qiniu_passwd
 
-$top_path/qiniu/qrsctl login $qiniu_user $qiniu_passwd
-
-echo $top_path
-dirs=`ls -R $top_path | grep ':' | awk -F: '{print $1}'`
+dirs=`ls -R $dl_dir | grep ':' | awk -F: '{print $1}'`
 for dir in $dirs; do
     if [ `basename $dir` = 'qiniu' ]; then
         continue
@@ -40,42 +41,50 @@ for dir in $dirs; do
     if [ ! -f index.md ]; then
         auto_index_md=1
         auto_files_md=0
-        if [ ! -f files.md ]; then
-            auto_files_md=1
-            echo 'filename|size' > files.md
-            echo '--------|----' >> files.md
-            tmp_lines=`ls --group-directories-first`
-            for filename in $tmp_lines; do
-                if [ -d $filename ]; then
-                    size=''
-                    filename="$filename""/"
-                else
-                    size=`ls -lh $filename | awk '{print $5}'`
-                fi
-                if [ $filename = 'files.md' ] || [ $filename = 'origin.md' ] || [ $filename = 'index.html' ] || [ $filename = 'index.md' ] || [ $filename = 'README.md' ]; then
-                    continue
-                fi
-                echo $filename'|'$size >> files.md
-            done
+        if [ ! -f dirs_and_files.md ]; then
+            auto_dirs_and_files_md=1
+            if [ ! -f dirs.md ]; then
+                auto_dirs_md=1
+                echo 'filename|size|md5' > dirs.md
+                echo '--------|----|---' >> dirs.md
+                tmp_lines=`ls --group-directories-first`
+                for filename in $tmp_lines; do
+                    if [ -d $filename ]; then
+                        filename="$filename""/"
+                        filename_for_markdown=${filename//_/\\_}
+                        filename_for_sed=${filename_for_markdown//\//\\/}
+                        echo $filename_for_markdown'||' >> dirs.md
+                    fi
+                done
+            fi
+            cp dirs.md dirs_and_files.md
+            if [ $auto_dirs_md -eq 1 ]; then
+                rm dirs.md
+            fi
+            if [ -f files.md ]; then
+                tail -n +3 files.md >> dirs_and_files.md
+            fi
         fi
-        thead_line_num=`grep -n "\-|\-" files.md | awk -F: '{print $1}'`
-        head -n $thead_line_num files.md > index.md
-        if [ $dir != $top_path ]; then
+
+        head -n 2 dirs_and_files.md > index.md
+        if [ $dir != $dl_dir ]; then
             echo '[../](../)|' >> index.md
         fi
-        offset=$(($thead_line_num+1))
-        tail -n +$offset files.md | while read line; do
+        tail -n +3 dirs_and_files.md | while read line; do
             filename=`echo $line | awk -F\| '{print $1}'`
+            filename_for_markdown=${filename//_/\\_}
             part2=`echo $line | awk -F\| '{print $2}'`
+            part3=`echo $line | awk -F\| '{print $3}'`
             # 把文件名都改成链接
-            echo '['$filename']('$filename')|'$part2 >> index.md
+            echo '['$filename_for_markdown']('$filename_for_markdown')|'$part2'|'$part3 >> index.md
         done
-        if [ $auto_files_md -eq 1 ]; then
-            rm files.md
+        if [ $auto_dirs_and_files_md -eq 1 ]; then
+            rm dirs_and_files.md
         fi
-        #空格是为了可读性，机器不需要，所以把空格删除
-        sed -i 's/ | /|/g' index.md
     fi
+
+    #空格是为了可读性，机器不需要，所以把空格删除
+    sed -i 's/ | /|/g' index.md
 
     # sudo apt-get install discount
     markdown index.md > tmp-index-part.html
@@ -87,21 +96,17 @@ for dir in $dirs; do
     tmp=`cat tmp-index-part.html | tr '\n' '\f'`
     body=`echo $tmp | sed -e "s|<table>|<table class=\"pure-table pure-table-striped pure-table-horizontal\">|g"`
     rm tmp-*
-    path=`pwd | sed -e "s|$top_path||"`
-    sed -e "s|{title}|Index of $path/|g" -e "s|{body}|$body|g" $top_path/tpl.html | tr '\f' '\n' > index.html
-    if [ $dir = $top_path ]; then
-        # 首页有标题，不需要这个标题
-        sed -i '/<h1>Index of \/<\/h1>/d' index.html
-        title=`grep '<h1>' index.html | sed -e 's|h1|title|g'`
-        sed -i "s|<title>Index of /</title>|$title|g" index.html
-    fi
+    dl_dir_for_sed=${dl_dir//\//\\/}
+    path=`pwd | sed -e "s|$dl_dir_for_sed||"`
+    path_for_sed=${path//\//\\/}
+    sed -e "s|{title}|Index of $path_for_sed/|g" -e "s|{body}|$body|g" $top_dir/tpl.html | tr '\f' '\n' > index.html
     qiniu_prefix=${path:1}"/"
     if [ $qiniu_prefix = "/" ]; then
         qiniu_prefix=""
     fi
     echo $qiniu_prefix
     # 把index.html上传到 七牛的xxx/，用于列表服务
-    $top_path/qiniu/qrsctl put dl-gmirror-org "$qiniu_prefix" index.html
-    $top_path/qiniu/qrsctl cdn/refresh dl-gmirror-org http://dl.gmirror.org/$qiniu_prefix
-    rm index.html
+    #qrsctl put $bucket "$qiniu_prefix" index.html
+    #qrsctl cdn/refresh $bucket http://$domain/$qiniu_prefix
+    #rm index.html
 done
