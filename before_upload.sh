826578ba3201a0555602de07597795d88d63507e
sinkcup <sinkcup@163.com>
2015-07-18 15:07:01 +0800

    move shell from travis to file

---
+++ before_upload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget -O /tmp/qiniu-devtools-linux_amd64-current.tar.gz http://devtools.qiniu.io/qiniu-devtools-linux_amd64-current.tar.gz || exit 1
+mkdir qiniu
+tar -zxvf /tmp/qiniu-devtools-linux_amd64-current.tar.gz -C ./qiniu/ || exit 2
+wget -O /tmp/qshell.zip http://qdisk.qiniudn.com/qshell%20v1.4.5.zip
+unzip /tmp/qshell.zip -d ./qiniu/ || exit 2
+cp ./qiniu/qshell_linux_amd64 ./qiniu/qshell
+export PATH=$PATH:$PWD/qiniu/
