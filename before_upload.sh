2536eac27c7ecdb8e4d69600602b3dcc84114a20
sinkcup <sinkcup@163.com>
2015-12-02 11:36:45 +0800

    fixbug: bad links

--- before_upload.sh
+++ before_upload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget -O /tmp/qiniu-devtools-linux_amd64-current.tar.gz http://devtools.qiniu.io/qiniu-devtools-linux_amd64-current.tar.gz || exit 1
+wget -O /tmp/qiniu-devtools-linux_amd64-current.tar.gz http://devtools.qiniu.com/qiniu-devtools-linux_amd64-current.tar.gz || exit 1
 mkdir qiniu
 tar -zxvf /tmp/qiniu-devtools-linux_amd64-current.tar.gz -C ./qiniu/ || exit 2
-wget -O /tmp/qshell.zip http://qdisk.qiniudn.com/qshell-v1.4.6.zip
+wget -O /tmp/qshell.zip http://devtools.qiniu.com/qshell-v1.6.3.zip
 unzip /tmp/qshell.zip -d ./qiniu/ || exit 2
 cp ./qiniu/qshell_linux_amd64 ./qiniu/qshell
