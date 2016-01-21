59f65284cf2514e7bd23fd3f83d2be9002b29683
sinkcup <sinkcup@163.com>
2016-01-21 16:54:28 +0800

    add portal

---
+++ portal.build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+top_dir=$(cd `dirname $0`; pwd)
+echo $top_dir
+source $top_dir/gmirror.conf
+dl_dir=$top_dir/dl
+cp $top_dir/portal/index.tpl.html $top_dir/portal/index.html
+
+cd $dl_dir
+files=`find $dl_dir -name 'origin.md'`
+for file in $files; do
+    absolute_dir=`dirname $file`
+    echo 'absolute_dir '$absolute_dir
+    relative_dir=`echo $file | sed -e "s|$dl_dir||" | xargs dirname`
+    echo 'relative_dir '$relative_dir
+
+    thead_line_num=`grep -n "\-|\-" $file | awk -F: '{print $1}'`
+    offset=$(($thead_line_num+1))
+    tail -n +$offset $file | while read line; do
+        i=0
+        new_line=""
+        echo -e "\n"$line
+        for part in `echo $line | sed 's/|/ /g'`; do
+            echo $part
+            #第1列是id
+            if [ $i -eq 0 ]; then
+                id=$part
+            elif [ $i -eq 1 ]; then
+                #第2列必须是下载地址
+                uri=$part
+                origin_filename=`basename $uri`
+                filename=$origin_filename
+                echo 'filename '$filename
+                uri_nopro=${part#*//}
+                target_path=${uri_nopro#*/}
+                echo 'ttt '$target_path
+            elif [ $i -eq 2 ]; then
+                #第3列必须是文件名或路径，如果为空的话，将使用下载地址里相同的文件名
+                filename=`basename $part`
+                target_path=${relative_dir#*/}/$part
+                echo 't2222 '$target_path
+                if [ $relative_dir == '/' ]; then
+                    target_path=$part
+                fi
+                echo 't333 '$target_path
+            fi
+            i=$(($i+1))
+        done
+        sed -i "s|{$id-filename}|$filename|g" $top_dir/portal/index.html
+        origin_domain=`echo $uri | awk -F '/' '{print $3}'`
+        sed -i "s|{$id-origin-uri}|$uri|g" $top_dir/portal/index.html
+        sed -i "s|{$id-origin-domain}|$origin_domain|g" $top_dir/portal/index.html
+        sed -i "s|{$id-gmirror-uri}|http://$qiniu_domain/$target_path|g" $top_dir/portal/index.html
+        sed -i "s|{$id-gmirror-domain}|$qiniu_domain|g" $top_dir/portal/index.html
+        continue;
+    done
+done
+echo 'the end'
+exit
---
+++ portal.upload.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+set -e
+top_dir=$(cd `dirname $0`; pwd)
+echo $top_dir
+portal_dir=$top_dir/portal
+source $top_dir/portal.conf
+
+# 如果环境变量里没有，则要输入
+if [ -z $qiniu_user ]; then
+    echo "请输入七牛用户名："
+    read -s qiniu_user
+    if [ -z $qiniu_user ]; then
+        echo '错误：未输入'
+        exit 1
+    fi
+fi
+
+if [ -z $qiniu_passwd ]; then
+    echo "请输入七牛密码："
+    read -s qiniu_passwd
+    if [ -z $qiniu_passwd ]; then
+        echo '错误：未输入'
+        exit 1
+    fi
+fi
+
+if [ -z $qiniu_access_key ]; then
+    echo "请输入七牛access_key："
+    read -s qiniu_access_key
+    if [ -z $qiniu_access_key ]; then
+        echo '错误：未输入'
+        exit 1
+    fi
+fi
+
+if [ -z $qiniu_secret_key ]; then
+    echo "请输入七牛secret_key："
+    read -s qiniu_secret_key
+    if [ -z $qiniu_secret_key ]; then
+        echo '错误：未输入'
+        exit 1
+    fi
+fi
+
+qrsctl login $qiniu_user $qiniu_passwd
+qshell account $qiniu_access_key $qiniu_secret_key
+
+dirs=`ls -R $portal_dir | grep ':' | awk -F: '{print $1}'`
+for dir in $dirs; do
+    echo $dir
+    cd $dir
+
+    path=`pwd | sed -e "s|$portal_dir||"`
+    qiniu_prefix=${path:1}"/"
+    if [ $qiniu_prefix = "/" ]; then
+        qiniu_prefix=""
+    fi
+    echo $qiniu_prefix
+    tmp_lines=`find . -maxdepth 1 -type f -printf '%f\n'`
+    for filename in $tmp_lines; do
+        echo $filename
+        echo $qiniu_prefix
+        target="$qiniu_prefix"$filename
+        if [ $filename == "index.html" ]; then
+            target=$qiniu_prefix
+        fi
+        echo $target
+        mime=`file -b --mime-type $filename`
+        echo $mime
+        qshell delete $qiniu_bucket $target
+        qshell fput $qiniu_bucket $target $filename "$mime" "http://upws.qiniug.com"
+        qrsctl cdn/refresh $qiniu_bucket http://$qiniu_domain/$target
+    done
+done
+rm $portal_dir/index.html
+echo 'the end'
