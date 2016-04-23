48839d608775bfc6869e8bc868620d4d2722028f
sinkcup <sinkcup@163.com>
2016-04-23 20:02:24 +0800

    fixbug: crash when grep not match

--- portal.build.sh
+++ portal.build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 mkdocs build --clean
-grep -lr "href='https://fonts.googleapis.com/" ./site | xargs sed -i "s/href='https:\/\/fonts\.googleapis\.com/href='http:\/\/fonts.gmirror.org/g"
+grep -lr "href='https://fonts.googleapis.com/" ./site/css/ && (grep -lr "href='https://fonts.googleapis.com/" ./site/css/ | xargs sed -i "s/href='https:\/\/fonts\.googleapis\.com/href='http:\/\/fonts\.gmirror\.org/g") || echo 'replace nothing'
+grep -lr "//fonts.googleapis.com/" ./site/css/ && (grep -lr "//fonts.googleapis.com/" ./site/css/ | xargs sed -i "s/\/\/fonts\.googleapis\.com/http:\/\/fonts\.gmirror\.org/g") || echo 'replace nothing'
