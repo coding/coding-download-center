07b1056ee7ee4b352aa951a10c978cdf948ae6f4
sinkcup <sinkcup@163.com>
2016-04-27 16:24:35 +0800

    debug: print md5

--- before_upload.sh
+++ before_upload.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 wget -O /tmp/qiniu-devtools-linux_amd64-current.tar.gz http://devtools.qiniu.com/qiniu-devtools-linux_amd64-current.tar.gz || exit 1
+md5sum /tmp/qiniu-devtools-linux_amd64-current.tar.gz
 mkdir qiniu
 tar -zxvf /tmp/qiniu-devtools-linux_amd64-current.tar.gz -C ./qiniu/ || exit 2
 wget -O /tmp/qshell.zip http://devtools.qiniu.com/qshell-v1.6.5.zip
+md5sum /tmp/qshell.zip
 unzip /tmp/qshell.zip -d ./qiniu/ || exit 2
 cp ./qiniu/qshell_linux_amd64 ./qiniu/qshell
