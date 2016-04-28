6a0db70509bd68a59fddddc12a44a44e5601099b
sinkcup <sinkcup@163.com>
2016-04-28 12:33:39 +0800

    fixbug: user can not see new css, because browser cache

--- portal.build.sh
+++ portal.build.sh
@@ -5,3 +5,10 @@ mkdocs build --clean
 grep -lr "href='https://fonts.googleapis.com/css" ./site/ && (grep -lr "href='https://fonts.googleapis.com/css" ./site/ | xargs sed -i "s/href='https:\/\/fonts\.googleapis\.com\/css/href='https:\/\/fonts\.gmirror\.org\/css/g") || echo 'replace nothing'
 # some theme use import in css file
 grep -lr "//fonts.googleapis.com/css" ./site/css/ && (grep -lr "//fonts.googleapis.com/css" ./site/css/ | xargs sed -i "s/\/\/fonts\.googleapis\.com\/css/\/\/fonts\.gmirror\.org\/css/g") || echo 'replace nothing'
+
+# change css file uri, else it will cached by browser
+for file in `find ./site/css/ -name '*.css'`; do
+    md5=`md5sum $file | awk '{print $1}'`
+    filename=`basename $file`
+    grep -lr "css/$filename\"" ./site/ && (grep -lr "css/$filename\"" ./site/ | xargs sed -i "s/css\/$filename\"/css\/$filename?md5=$md5\"/g") || echo 'replace nothing'
+done
