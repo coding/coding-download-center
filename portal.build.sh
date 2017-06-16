9b5624a257d70656fd7546c3550241028e65987d
sinkcup <sinkcup@live.it>
2017-06-16 11:16:44 +0800

    fixbug: use ust proxy of google fonts

--- portal.build.sh
+++ portal.build.sh
@@ -2,9 +2,9 @@
 
 mkdocs build --clean
 # some theme use link css in HTML head
-grep -lr "href='https://fonts.googleapis.com/css" ./site/ && (grep -lr "href='https://fonts.googleapis.com/css" ./site/ | xargs sed -i "s/href='https:\/\/fonts\.googleapis\.com\/css/href='https:\/\/fonts\.gmirror\.org\/css/g") || echo 'replace nothing'
+grep -lr "href='https://fonts.googleapis.com/css" ./site/ && (grep -lr "href='https://fonts.googleapis.com/css" ./site/ | xargs sed -i "s/href='https:\/\/fonts\.googleapis\.com\/css/href='https:\/\/fonts\.proxy\.ustclug\.org\/css/g") || echo 'replace nothing'
 # some theme use import in css file
-grep -lr "//fonts.googleapis.com/css" ./site/css/ && (grep -lr "//fonts.googleapis.com/css" ./site/css/ | xargs sed -i "s/\/\/fonts\.googleapis\.com\/css/\/\/fonts\.gmirror\.org\/css/g") || echo 'replace nothing'
+grep -lr "//fonts.googleapis.com/css" ./site/css/ && (grep -lr "//fonts.googleapis.com/css" ./site/css/ | xargs sed -i "s/\/\/fonts\.googleapis\.com\/css/\/\/fonts\.proxy\.ustclug\.org\/css/g") || echo 'replace nothing'
 
 # change css file uri, else it will cached by browser
 for file in `find ./site/css/ -name '*.css'`; do
