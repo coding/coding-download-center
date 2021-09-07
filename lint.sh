233fa272f90771639c1c920e17b02ea11fd4cc55
shenwenjing <shenwenjing@coding.net>
2021-09-07 15:17:43 +0800

    feat(lint.sh): #43

--- lint.sh
+++ lint.sh
@@ -5,3 +5,9 @@ tail -n +3 index.md > /tmp/index-body.md
 cd /tmp
 cat index-body.md | sort > index-body-sorted.md 
 diff index-body.md index-body-sorted.md
+
+for i in `awk '{print $1}' /tmp/index-body.md`
+do
+  [[ "$i" =~ ^[a-z0-9.-]+$ ]]  || ( echo “$i: Product names shall be all in lower case” && exit 250 )
+done
+
