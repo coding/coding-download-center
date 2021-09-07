cbddd51750ca675114b0e718b9eba083d80cc5ed
YHemin <240190523@qq.com>
2021-09-07 22:44:24 +0800

    fix: 🐛 add index-body sorted remind

--- lint.sh
+++ lint.sh
@@ -5,3 +5,6 @@ tail -n +3 index.md > /tmp/index-body.md
 cd /tmp
 cat index-body.md | sort > index-body-sorted.md 
 diff index-body.md index-body-sorted.md
+if [ $? -ne 0 ]; then
+    echo "skip: software is not in alphabetical order"
+fi
