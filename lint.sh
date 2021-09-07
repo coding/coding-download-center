60d5559369f5bd62ab5bd116b020799b4ca09d52
Hot_Rain <65548078+Hot-Rain1@users.noreply.github.com>
2021-09-07 16:07:51 +0800

    feat : #42 package name should not have version

--- lint.sh
+++ lint.sh
@@ -7,6 +7,7 @@ cat index-body.md | sort > index-body-sorted.md
 diff index-body.md index-body-sorted.md
 
 package_name=$(awk '{print $1}' index-body.md)
+
 for i in $package_name;do
     [[ ! "$i" =~ \.[0-9]+\. ]] || ( echo "[ERROR] $i : package name should not have version!" && exit 2 )
 done
