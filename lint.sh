88f90a0d28196c1d27a798d612a5773dd6f96eab
hurui <hurui@coding.net>
2021-09-07 14:56:05 +0800

    feat(lint.sh): #42

--- lint.sh
+++ lint.sh
@@ -5,3 +5,8 @@ tail -n +3 index.md > /tmp/index-body.md
 cd /tmp
 cat index-body.md | sort > index-body-sorted.md 
 diff index-body.md index-body-sorted.md
+
+package_name=$(awk '{print $1}' index-body.md)
+for i in $package_name;do
+	[[ ! "$i" =~ \.[0-9]+\. ]] || ( echo "[ERROR] $i : package name should not have version!" && exit 2 )
+done
