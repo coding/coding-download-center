dbf2926c579da27133ef0a33ecd655f757c3c59a
sinkcup <sinkcup@gmail.com>
2021-09-08 15:59:04 +0800

    style: #57 shell style

--- lint.sh
+++ lint.sh
@@ -3,5 +3,5 @@ set -e
 
 tail -n +3 index.md > /tmp/index-body.md
 cd /tmp
-cat index-body.md | sort > index-body-sorted.md 
+sort index-body.md > index-body-sorted.md
 diff index-body.md index-body-sorted.md
