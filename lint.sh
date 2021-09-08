a96ae2cccfbc09f818edea7e32118c240adde49b
sinkcup <sinkcup@gmail.com>
2021-09-08 18:01:47 +0800

    ci: #57 lint shell

--- lint.sh
+++ lint.sh
@@ -1,7 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-tail -n +3 index.md > /tmp/index-body.md
-cd /tmp
-sort index-body.md > index-body-sorted.md
-diff index-body.md index-body-sorted.md
+if [ -z "$1" ]; then
+    echo "no file"
+    exit
+fi
+
+echo "lint shell"
+shell_files=$(echo "$@" | tr ' ' '\n' | { grep ".sh$" || true; })
+if [ -n "$shell_files" ]; then
+    echo "$shell_files" | xargs shellcheck
+    echo "$shell_files" | xargs shfmt -d -i 4 -sr
+else
+    echo "no shell file"
+fi
+
+index_file=$(echo "$@" | tr ' ' '\n' | { grep "^index.md$" || true; })
+if [ -n "$index_file" ]; then
+    echo "lint index.md"
+    echo "check package name sort by a-z"
+    tail -n +3 index.md > /tmp/index-body.md
+    cd /tmp
+    sort index-body.md > index-body-sorted.md
+    diff index-body.md index-body-sorted.md
+fi
+
+## lint markdown
+# TODO
